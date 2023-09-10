d9a1ffa35a39860a4b10309945e75c72dba55a8e
Tokiosman80F <tokiosman96@gmail.com>
2023-09-11 01:08:36 +0600

    data created for individual destination

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const destination=require('./data/destination.json')
+const hotelData=require('./data/hotel.json')
 app.get("/", (req, res) => {
   res.send("Hello Travel wise!");
 });
+
+
+app.get('/destination',(req,res)=>{
+  console.log("destination data");
+  res.send(destination)
+})
+// whole hotel data
+app.get("/hotelData",(req,res)=>{
+  res.send(hotelData)
+})
+// filter data according to cate
+app.get('/hotelData/:id',(req,res)=>{
+  const id=req.params.id;
+  const selectedData=hotelData.filter(data=>data.category_id==id)
+  console.log(selectedData);
+  res.send(selectedData)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
