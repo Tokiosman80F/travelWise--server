763c581ac582785543d1051043d728517f8bb1c0
Tokiosman80F <tokiosman96@gmail.com>
2023-09-11 14:51:45 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const port = 3000;
 const destination=require('./data/destination.json')
 const hotelData=require('./data/hotel.json')
+
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello Travel wise!");
 });
@@ -16,7 +19,8 @@ app.get('/destination',(req,res)=>{
 app.get("/hotelData",(req,res)=>{
   res.send(hotelData)
 })
-// filter data according to cate
+// filter data according to category
+// specific data
 app.get('/hotelData/:id',(req,res)=>{
   const id=req.params.id;
   const selectedData=hotelData.filter(data=>data.category_id==id)
